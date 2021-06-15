080c768fa2e58ab55e6dd452ea68e6f9871a73cc
Okafor Chidimma <chidimma.okafor.c@gmail.com>
2021-06-15 20:33:31 +0100

    Post CRUD Resolver

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import express from "express";
 import { MikroORM } from "@mikro-orm/core";
 import { ApolloServer } from "apollo-server-express";
@@ -7,18 +8,22 @@ import { __prod__ } from "./constants";
 import config from "./mikro-orm.config";
 import PostResolver from "./resolvers/post";
 
+
 const main = async () => {
   //db configuration
   const orm = await MikroORM.init(config);
   //run migrations automatically
   await orm.getMigrator().up();
+  
   //setup the express server
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [PostResolver],
+      //turned off validator so that the default class-validator package that type graphql uses will not be used for us
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
   apolloServer.applyMiddleware({ app });
   app.listen(3000, () => {
